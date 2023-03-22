af0bbc3e3f6ffafab1fddc60af1558ff8152adf6
LewisDyer <lewisd251@gmail.com>
2023-03-22 15:35:40 +0000

    Tried timeout for radius > 2, still very slow

--- local_treewidth.py
+++ local_treewidth.py
@@ -1,9 +1,19 @@
 import networkx as nx
 from networkx.algorithms.approximation import treewidth_min_degree
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 import lg_parser
 import patent_parser
+
+def process_local(G, i, v, radius, tws, dens, degs):
+    ball = nx.ego_graph(G, v, radius)
+    tw = treewidth_min_degree(ball)[0]
+    tws[i] = tw
+    dens[i] = nx.density(ball)
+    degs[i] = G.degree(v)
+    if i % 10 == 0:
+        print(i)
 
 def local_treewidth(G, radius):
 
@@ -11,14 +21,15 @@ def local_treewidth(G, radius):
     densities = [0] * len(G.nodes())
     degrees = [0] * len(G.nodes())
     for i, v in enumerate(G.nodes()):
-        ball = nx.ego_graph(G, v, radius)
-        tw = treewidth_min_degree(ball)[0]
-        treewidths[i] = tw
-        densities[i] = nx.density(ball)
-        degrees[i] = G.degree(v)
-        if i % 1000 == 0:
-            print(i)
-        #print(tw)
+
+        process = mp.Process(target=process_local, args=(G,i,v,radius,treewidths,densities,degrees))
+        process.start()
+        process.join(timeout=1)
+
+        if process.is_alive():
+            process.terminate()
+            process.join()
+            print(f"timeout at i={i}")
     
     return(treewidths, densities, degrees)
     # for vertex in G:
@@ -31,16 +42,16 @@ if __name__ == '__main__':
 
     #nx.draw(G)
     #plt.show()
-    #G = lg_parser.parse_lg('datasets/mico.lg')
-    G = patent_parser.parse_patents('datasets/cit-Patents.txt')
-    tws, dens, degs = local_treewidth(G, 1)
-    f = open("outputs/patents_tws.txt", "w")
+    G = lg_parser.parse_lg('datasets/mico.lg')
+    #G = patent_parser.parse_patents('datasets/cit-Patents.txt')
+    tws, dens, degs = local_treewidth(G, 2)
+    f = open("outputs/mico2_tws.txt", "w")
     for tw in tws:
         f.write(f"{tw}\n")
-    f = open("outputs/patents_density.txt", "w")
+    f = open("outputs/mico2_density.txt", "w")
     for den in dens:
         f.write(f"{den}\n")
-    f = open("outputs/patents_degrees.txt", "w")
+    f = open("outputs/mico2_degrees.txt", "w")
     for deg in degs:
         f.write(f"{deg}\n")
 
