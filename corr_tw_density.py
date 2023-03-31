0fe0ce43606480982f87fb35f6385336f560bada
LewisDyer <lewisd251@gmail.com>
2023-03-31 09:47:45 +0100

    Added degeneracy + tried random baselines

--- corr_tw_density.py
+++ corr_tw_density.py
@@ -5,27 +5,32 @@ tws = []
 densities = []
 degrees = []
 clusters = []
-with open("outputs/mico_tws.txt") as tw:
+cores = []
+with open("outputs/random_patents_tws.txt") as tw:
     for line in tw:
         tws.append(float(line.strip()))
 
-with open("outputs/mico_density.txt") as dens:
+with open("outputs/random_patents_density.txt") as dens:
     for line in dens:
         densities.append(float(line.strip()))
 
-with open("outputs/mico_degrees.txt") as degs:
+with open("outputs/random_patents_degrees.txt") as degs:
     for line in degs:
         degrees.append(int(line.strip()))
 
-with open("outputs/mico_clustering.txt") as clus:
+with open("outputs/random_patents_clustering.txt") as clus:
     for line in clus:
         clusters.append(float(line.strip()))
+
+with open("outputs/random_patents_cores.txt") as cors:
+    for line in cors:
+        cores.append(int(line.strip()))
 log_degrees = [math.log(d, 2) if d > 0 else 0 for d in degrees]
 
 
-fig, ax = plt.subplots(4, figsize=(15,15))
+fig, ax = plt.subplots(5, figsize=(15,15))
 
-fig.suptitle('Comparison between local density parameters for mico dataset')
+fig.suptitle('Comparison between local density parameters for mico-like dataset')
 
 ax[0].scatter(tws, densities, marker='.')
 ax[0].set_xlabel("local treewidth")
@@ -43,5 +48,9 @@ ax[3].scatter(densities, clusters, marker='.')
 ax[3].set_xlabel("local edge density")
 ax[3].set_ylabel("local clustering coefficient of starting vertex")
 
+ax[4].scatter(tws, cores, marker='.')
+ax[4].set_xlabel("local treewidth")
+ax[4].set_ylabel("local degeneracy")
+
 #sc = ax.scatter(tws, degrees,  marker='.')
-plt.savefig('figures/mico_density.jpg', dpi=300)
+plt.savefig('figures/random_patents_density.jpg', dpi=300)
--- local_treewidth.py
+++ local_treewidth.py
@@ -12,6 +12,7 @@ def local_treewidth(G, radius):
     densities = [0] * len(G.nodes())
     degrees = [0] * len(G.nodes())
     clustering = [0] * len(G.nodes())
+    cores = [0] * len(G.nodes())
 
     clusters = nx.clustering(G)
     for i, v in enumerate(G.nodes()):
@@ -20,12 +21,15 @@ def local_treewidth(G, radius):
         tw = treewidth_min_degree(ball)[0]
         treewidths[i] = tw
         densities[i] = nx.density(ball)
-        degrees[i] = G.degree(v)
+        degrees[i] = ball.degree(v)
         clustering[i] = clusters[v]
+        clear_ball = ball.remove_edges_from(nx.selfloop_edges(ball))
+        if clear_ball is not None:
+            cores[i] = min(nx.core_number(clear_ball).values())
         if i % 1000 == 0:
             print(i)
     
-    return(treewidths, densities, degrees, clustering)
+    return(treewidths, densities, degrees, clustering, cores)
     # for vertex in G:
     # get ball of defined radius around vertex
     # find treewidth of this ball
@@ -36,22 +40,29 @@ if __name__ == '__main__':
 
     #nx.draw(G)
     #plt.show()
-    G = lg_parser.parse_lg('datasets/mico.lg')
+    #G = lg_parser.parse_lg('datasets/mico.lg')
     #G = patent_parser.parse_patents('datasets/cit-Patents.txt')
-    tws, dens, degs, clus = local_treewidth(G, 1)
-    f = open("outputs/mico_tws.txt", "w")
+    #G = nx.gnm_random_graph(100000,1080156) # mico like
+    G = nx.gnm_random_graph(3774768, 16518948) # patents like
+    tws, dens, degs, clus, cores = local_treewidth(G, 1)
+    f = open("outputs/random_patents_tws.txt", "w")
     for tw in tws:
         f.write(f"{tw}\n")
-    f = open("outputs/mico_density.txt", "w")
+    f = open("outputs/random_patents_density.txt", "w")
     for den in dens:
         f.write(f"{den}\n")
-    f = open("outputs/mico_degrees.txt", "w")
+    f = open("outputs/random_patents_degrees.txt", "w")
     for deg in degs:
         f.write(f"{deg}\n")
     
-    f = open("outputs/mico_clustering.txt", "w")
+    f = open("outputs/random_patents_clustering.txt", "w")
     for clu in clus:
         f.write(f"{clu}\n")
+
+    f = open("outputs/random_patents_cores.txt", "w")
+    for cor in cores:
+        f.write(f"{cor}\n")
+
     
     f.close()
     
